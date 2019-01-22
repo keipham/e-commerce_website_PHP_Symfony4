b27d3eabe1807ba0b903baa6d5af2271247b80ff
Sylvie Godard <sylvie.godard@coding-academy.fr>
2019-01-22 22:32:15 +0100

    ajout image ajout entity game, pas encore de migration ajout "composer require server --dev"

---
+++ netEscape/src/Entity/Games.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GamesRepository")
+ */
+class Games
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $playerMin;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $playerMax;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getPlayerMin(): ?int
+    {
+        return $this->playerMin;
+    }
+
+    public function setPlayerMin(int $playerMin): self
+    {
+        $this->playerMin = $playerMin;
+
+        return $this;
+    }
+
+    public function getPlayerMax(): ?int
+    {
+        return $this->playerMax;
+    }
+
+    public function setPlayerMax(int $playerMax): self
+    {
+        $this->playerMax = $playerMax;
+
+        return $this;
+    }
+}
