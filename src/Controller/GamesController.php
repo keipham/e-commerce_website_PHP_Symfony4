e1bc2cb62122fe554bc8a310cf8a857dc37e170e
Sylvie Godard <sylvie.godard@coding-academy.fr>
2019-01-30 13:26:31 +0100

    test phpspec sur des fonctions lambda

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -48,4 +48,18 @@ class GamesController extends AbstractController
         ]);
     }
 
+    public function add($argument1, $argument2)
+    {
+        return $argument1 + $argument2;
+    }
+
+    public function divide($numerator, $denominator)
+    {
+        if ($denominator === 0) {
+            throw new DivisionByZeroException();
+        }
+    
+        return $numerator / $denominator;
+    }
+
 }
