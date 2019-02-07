e3e4523ec16546e8bb5cf5f3a94d57edb23d6bce
Sylvie Godard <sylvie.godard@coding-academy.fr>
2019-02-07 13:10:23 +0100

    bug bouton modifier non réglé

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Image;
-use App\Form\ImageType;
+use App\Form\Image1Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,9 +28,72 @@ class ImageController extends AbstractController
         ]);
     }
 
-    
+    /**
+     * @Route("/new", name="image_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(Image1Type::class, $image);
+        $form->handleRequest($request);
 
-    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($image);
+            $entityManager->flush();
 
-  
+            return $this->redirectToRoute('image_index');
+        }
+
+        return $this->render('image/new.html.twig', [
+            'image' => $image,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="image_show", methods={"GET"})
+     */
+    public function show(Image $image): Response
+    {
+        return $this->render('image/show.html.twig', [
+            'image' => $image,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="image_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Image $image): Response
+    {
+        $form = $this->createForm(Image1Type::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('image_index', [
+                'id' => $image->getId(),
+            ]);
+        }
+
+        return $this->render('image/edit.html.twig', [
+            'image' => $image,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="image_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Image $image): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($image);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('image_index');
+    }
 }
